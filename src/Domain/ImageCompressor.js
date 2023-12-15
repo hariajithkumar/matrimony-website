a5f584ed04a64bbaa14fd9892d4cd7e57adf088f
hariajithkumar <ajithkumar260598@gmail.com>
2023-12-15 19:43:26 +0530

    compressImage

---
+++ src/Domain/ImageCompressor.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import imageCompression from "browser-image-compression";
+import upload from '../Common/image/upload.png'
+import verify from '../Common/image/verifydetails.png'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
+
+
+
+
+const ImageCompressor = () => {
+  const [compressedImages, setCompressedImages] = useState([]);
+
+  const compressImage = async (file) => {
+    const imageDimensions = await (file);
+    // const imageDimensions = await imageSize(file);
+
+    // console.log({ imageDimensions, size: niceBytes(file.size) });
+
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight:
+        imageDimensions?.width > 1300 ? 1300 : imageDimensions?.width,
+      useWebWorker: true,
+    };
+
+    const compressedImg = await imageCompression(file, options);
+
+    return compressedImg;
+  };
+
+  const uploadImage = async (files) => {
+    const compressedImage = await compressImage(files[0]);
+    console.log(1, compressedImage.name)
+    setCompressedImages((prevImages) => [...prevImages, compressedImage]);
+  };
+  const deleteitem = (id) => {
+    const idToRemove = id; // Replace with the actual 'id' you want to remove
+    const updatedFiles = compressedImages.filter((file, index) => index !== idToRemove);
+    setCompressedImages(updatedFiles);
+  };
+  console.log(compressedImages)
+  return (
+    <div>
+      <form>
+        {/* <input
+            className="form-control"
+            type="file"
+            onChange={(e) => uploadImage(e.target.files)}
+          /> */}
+        <input type="file" id="upload" onChange={(e) => uploadImage(e.target.files)} style={{ display: 'none' }} />
+        <label htmlFor="upload" className="custom-file-upload">
+          <img src={upload} alt="upload" className="w-100 h-100" />
+        </label>
+      </form>
+      <div className='row m-0 py-3'>
+        {/* {previewUrls.map((previewUrl, index) => (
+          <div className='col-4 mb-2'>
+            <img key={index} src={previewUrl} alt="preview" className='w-100 card-views p-0' />
+            <button type="button" className="close position-absolute border-0 bg-none close-btn" onClick={() => deleteitem(index)}>
+              <FontAwesomeIcon icon={faClose} />
+            </button>
+          </div>
+        ))} */}
+        <div className='col-6 mb-2'>
+          {compressedImages.map((image, index) => (
+            <>
+              {/* <p>Compressed Image {index + 1}:</p> */}
+              <img src={URL.createObjectURL(image)} alt={`Compressed ${index + 1}`} className="w-100"/>
+              {/* <p>Size: {niceBytes(image.size)}</p> */}
+              <button type="button" className="close position-absolute border-0 bg-none close-btn" onClick={() => deleteitem(index)}>
+                <FontAwesomeIcon icon={faClose} />
+              </button>
+              </>
+          ))}
+        </div>
+      </div>
+      {/* <div className='proof-verified mt-4'>
+        {compressedImages.length > 1 ? <><img src={verify} /><span className='mx-2'>Verified</span><div className='text-center save-btn'><button type='button' onClick={() => proofupdate()}>Upload</button></div></> : ""}
+      </div> */}
+
+    </div>
+  );
+};
+
+export default ImageCompressor;
--- src/Domain/Proofverify.js
+++ src/Domain/Proofverify.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import imageCompression from "browser-image-compression";
+
+
 import Profileheader from '../Common/page/Profileheader'
 import Footer from '../Common/page/Footer'
 import Useraside from '../Common/page/Useraside'
@@ -6,55 +9,137 @@ import Benifit from '../Common/page/Benefit'
 import '../Common/css/profile.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClose, faL } from '@fortawesome/free-solid-svg-icons';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
 
 import upload from '../Common/image/upload.png'
 import editprofile from '../Common/image/white-edit.png'
 import verify from '../Common/image/verifydetails.png'
+import ImageCompressor from './ImageCompressor';
+// import ImageUploader from './ImageUploader';
+
+
+// compress fn code 
+
+const units = ["bytes", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+
+function niceBytes(x) {
+    let l = 0,
+        n = parseInt(x, 10) || 0;
+
+    while (n >= 1024 && ++l) {
+        n = n / 1024;
+    }
+
+    return n.toFixed(n < 10 && l > 0 ? 1 : 0) + " " + units[l];
+}
+
+const imageSize = (file) => {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    const promise = new Promise((resolve, reject) => {
+        reader.onload = function (e) {
+            const image = new Image();
+
+            image.src = e.target.result;
+
+            image.onload = function () {
+                const height = this.height;
+                const width = this.width;
+
+                resolve({ width, height });
+            };
+
+            image.onerror = reject;
+        };
+    });
+
+    return promise;
+};
+
+
+
 
 function Proofverify() {
     const [selected, setSelected] = useState('');
     // State for storing file data and preview URLs
     const [files, setFiles] = useState([]);
     const [previewUrls, setPreviewUrls] = useState([]);
+    const [compressedImages, setCompressedImages] = useState([]);
+    const [base64Image, setBase64Image] = useState("");
 
     // Function to handle file uploads
-    const handleFileUpload = (e) => {
-        const uploadedFiles = Array.from(e.target.files);
-        console.log(uploadedFiles)
-        // Map each file to a Promise that resolves to an array of the file itself and its preview URL
-        const filePromises = uploadedFiles.map((file) => {
+    // const handleFileUpload = (e) => {
+    //     const uploadedFiles = Array.from(e.target.files);
+    //     console.log(uploadedFiles)
+    //     // Map each file to a Promise that resolves to an array of the file itself and its preview URL
+    //     const filePromises = uploadedFiles.map((file) => {
 
-            return new Promise((resolve) => {
-                const size = file.size;
+    //         return new Promise((resolve) => {
+    //             const size = file.size;
 
-                if (size > 100 * 102) {
-                    alert('file size above 100 KB')
-                } else {
-                    const reader = new FileReader();
-                    reader.onload = (e) => {
-                        resolve([file, e.target.result]);
-                        const previewUrl = e.target.result;
-                        resolve({ file, previewUrls, size });
-                        reader.readAsDataURL(file);
-                    };
-                    reader.readAsDataURL(file);
-                }
-            });
+    //             if (size > 100 * 102) {
+    //                 alert('file size above 100 KB')
+    //             } else {
+    //                 const reader = new FileReader();
+    //                 reader.onload = (e) => {
+    //                     resolve([file, e.target.result]);
+    //                     const previewUrl = e.target.result;
+    //                     resolve({ file, previewUrls, size });
+    //                     reader.readAsDataURL(file);
+    //                 };
+    //                 reader.readAsDataURL(file);
+    //             }
+    //         });
+    //     });
+
+    //     // Once all promises are resolved, update the state with the new files and preview URLs
+    //     Promise.all(filePromises).then((results) => {
+    //         console.log(results)
+    //         const newFiles = results.map((result) => result[0]);
+    //         const newPreviewUrls = results.map((result) => result[1]);
+    //         setFiles([...files, ...newFiles]);
+    //         setPreviewUrls([...previewUrls, ...newPreviewUrls])
+    //     })
+    //         .catch((error) => {
+    //             console.error('Error uploading file:', error.message);
+    //             // Handle the error, e.g., show a notification to the user
+    //         });
+    // };
+
+    // compressImage fn code 
+    const compressImage = async (file) => {
+        const reader = new FileReader();
+        const imageDimensions = await imageSize(file);
+        console.log({ imageDimensions, size: niceBytes(file.size) });
+
+        const options = {
+            maxSizeMB: 1,
+            maxWidthOrHeight: imageDimensions?.width > 1300 ? 1300 : imageDimensions?.width,
+            useWebWorker: true,
+        };
+
+        const compressedImg = await imageCompression(file, options);
+        console.log(6,niceBytes(compressedImg.size))
+        // Convert the compressed image to base64
+        const base64String = await convertToBase64(compressedImg);
+
+        return base64String;
+    };
+
+    const convertToBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = (error) => reject(error);
         });
+    };
 
-        // Once all promises are resolved, update the state with the new files and preview URLs
-        Promise.all(filePromises).then((results) => {
-            console.log(results)
-            const newFiles = results.map((result) => result[0]);
-            const newPreviewUrls = results.map((result) => result[1]);
-            setFiles([...files, ...newFiles]);
-            setPreviewUrls([...previewUrls, ...newPreviewUrls])
-        })
-            .catch((error) => {
-                console.error('Error uploading file:', error.message);
-                // Handle the error, e.g., show a notification to the user
-            });
+    const uploadImage = async (files) => {
+        const base64String = await compressImage(files[0]);
+        setPreviewUrls((prevImages) => [...prevImages, base64String]);
     };
 
     const handleChange = event => {
@@ -71,7 +156,7 @@ function Proofverify() {
         const updatedFiles = previewUrls.filter((file, index) => index !== idToRemove);
         setPreviewUrls(updatedFiles);
     };
-    console.log(previewUrls.length)
+    console.log(previewUrls)
     return (
         <>
             <Profileheader />
@@ -113,7 +198,7 @@ function Proofverify() {
                                                     <>
                                                         {previewUrls.length == 2 ? <></> :
                                                             <>
-                                                                <input type="file" id="upload" onChange={handleFileUpload} style={{ display: 'none' }} />
+                                                                <input type="file" id="upload" onChange={(e) => uploadImage(e.target.files)} style={{ display: 'none' }} />
                                                                 <label htmlFor="upload" className="custom-file-upload">
                                                                     <img src={upload} alt="upload" className="w-100 h-100" />
                                                                 </label>
@@ -125,6 +210,9 @@ function Proofverify() {
                                                     <></>
                                                 }
 
+                                            </div>
+                                            <div>
+                                                {/* <ImageCompressor /> */}
                                             </div>
                                             <div className='col-12'>
                                                 {selected == 'Voter' || selected == 'Aadhar' || selected == 'License' ? <><div className='proof-verify'><h2><b>Upload Front side and Back side image</b></h2></div></> : <></>}
@@ -146,14 +234,12 @@ function Proofverify() {
                                     </div>
                                 </div>
                             </div>
-
                         </div>
                         <div className='col-3'>
                             <Benifit />
                         </div>
                     </div>
                 </div>
-
             </section >
             <Footer />
         </>
